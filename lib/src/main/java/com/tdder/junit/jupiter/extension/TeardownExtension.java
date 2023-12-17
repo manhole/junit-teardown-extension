d4eb9c4e4edf76d02e15b9034c6be2198786893c
manhole <manholex@gmail.com>
2023-12-17 22:11:05 +0900

    Execute teardown logics added to TeardownRegistry

--- lib/src/main/java/com/tdder/junit/jupiter/extension/TeardownExtension.java
+++ lib/src/main/java/com/tdder/junit/jupiter/extension/TeardownExtension.java
@@ -1,11 +1,15 @@
 package com.tdder.junit.jupiter.extension;
 
+import java.util.Deque;
+import java.util.LinkedList;
+
+import org.junit.jupiter.api.extension.AfterEachCallback;
 import org.junit.jupiter.api.extension.ExtensionContext;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
 
-public class TeardownExtension implements ParameterResolver {
+public class TeardownExtension implements ParameterResolver, AfterEachCallback {
 
     private final ExtensionContext.Namespace namespace_ = ExtensionContext.Namespace.create(getClass());
 
@@ -25,11 +29,35 @@ public class TeardownExtension implements ParameterResolver {
                 TeardownRegistryImpl.class);
     }
 
+    @Override
+    public void afterEach(final ExtensionContext extensionContext) throws Exception {
+        final ExtensionContext.Store store = extensionContext.getStore(namespace_);
+        final TeardownRegistryImpl teardown = store.get(STORE_KEY, TeardownRegistryImpl.class);
+        if (teardown != null) {
+            teardown.teardown();
+        }
+    }
+
     static class TeardownRegistryImpl implements TeardownRegistry {
+
+        private final Deque<AutoCloseable> tasks_ = new LinkedList<>();
 
         @Override
         public <T extends AutoCloseable> T add(final T closeable) {
+            tasks_.add(closeable);
             return closeable;
+        }
+
+        public void teardown() {
+            while (!tasks_.isEmpty()) {
+                // teardown in reverse order
+                final AutoCloseable task = tasks_.removeLast();
+                try {
+                    task.close();
+                } catch (final Exception e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
     }
--- lib/src/test/java/com/tdder/junit/jupiter/extension/TeardownExtensionTest.java
+++ lib/src/test/java/com/tdder/junit/jupiter/extension/TeardownExtensionTest.java
@@ -1,10 +1,15 @@
 package com.tdder.junit.jupiter.extension;
 
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.platform.engine.discovery.DiscoverySelectors;
@@ -18,12 +23,20 @@ import org.junit.platform.launcher.listeners.TestExecutionSummary;
 
 class TeardownExtensionTest {
 
+    private static final List<String> messages = new ArrayList<>();
+
+    @BeforeEach
+    void setUp() {
+        messages.clear();
+    }
+
     @Test
     void methodInjection() throws Exception {
         final TestExecutionSummary summary = runTest(MethodInjection.class);
 
         assertEquals(0, summary.getTestsFailedCount());
         assertEquals(1, summary.getTestsSucceededCount());
+        assertThat(messages, is(contains("3", "2", "1")));
     }
 
     static TestExecutionSummary runTest(final Class<?> testClass) {
@@ -44,8 +57,12 @@ class TeardownExtensionTest {
     static class MethodInjection {
 
         @Test
-        void succeeded1(final TeardownRegistry teardown) throws Exception {
-            assertThat(teardown, is(notNullValue()));
+        void succeeded1(final TeardownRegistry teardownRegistry) throws Exception {
+            assertThat(teardownRegistry, is(notNullValue()));
+
+            teardownRegistry.add(() -> messages.add("1"));
+            teardownRegistry.add(() -> messages.add("2"));
+            teardownRegistry.add(() -> messages.add("3"));
 
             assertEquals(1, 1);
         }
